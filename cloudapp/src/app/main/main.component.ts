ccd06946a150375b66aebd5bd2a1488cf961c9f0
Mateusz Lasota <mateusz.lasota@pg.edu.pl>
2023-12-20 12:58:02 +0100

    refactoring (to three sub methods)

--- cloudapp/src/app/main/main.component.ts
+++ cloudapp/src/app/main/main.component.ts
@@ -15,8 +15,6 @@ export class MainComponent implements OnInit, OnDestroy {
   loading = false;
   selectedEntity: Entity;
   apiResult: any;
-  
-              
 
   constructor(
     private restService: CloudAppRestService,
@@ -30,50 +28,47 @@ export class MainComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
   }
-
+  ___parse_user_id(letter : string, unique_users_ids: Array<string>): string | void {
+    var parser = new DOMParser();
+    let user_primary_id_span_html = parser.parseFromString(letter,"text/html")
+                                        .getElementById('PrimaryID')?.innerHTML;
+    if (user_primary_id_span_html) {
+            return user_primary_id_span_html;
+    }
+  }
+  __parse_unique_users_ids(printouts: Array<any>): Array<number> {
+    let unique_users_ids = [];
+    printouts.forEach((printout) => {
+        let user_id = this.___parse_user_id(printout.letter, unique_users_ids)
+        if (user_id) {
+            unique_users_ids.push(user_id)
+        }
+    });
+    return unique_users_ids.filter((value, index, array) => array.indexOf(value) === index);
+  }
+  __fetch_books_barcodes(user_requests: Array<any>): Array<string> {
+    return user_requests
+        .filter(ur => ur.pickup_location_circulation_desk === "cd_remotelocker-requ")
+        .filter(ur => ur.task_name === "Transit Item")
+        .reduce((user_requests,book) => {user_requests.push(book.barcode); return user_requests;}, []);
+  }
   _get_requested_resources():void {
     let extracted_datas = [];
-    let unique_users_primary_id = [];
     this.restService.call<any>('/almaws/v1/task-lists/printouts?letter=Transit Letter&limit=100')
         .pipe(
-        map(printouts => printouts.printout),
-        map(printouts => {
-            printouts.forEach((printout) => {
-                var parser = new DOMParser();
-                var printout_dom = parser.parseFromString(printout.letter,"text/html");
-                let user_primary_id_span = printout_dom.getElementById('PrimaryID');
-                if (user_primary_id_span?.innerHTML) {
-                    var user_primary_id = user_primary_id_span.innerHTML;
-                    if (!unique_users_primary_id.includes(user_primary_id)) {
-                        unique_users_primary_id.push(user_primary_id);
-                    }
-                }
-            });
-            return unique_users_primary_id;
-        }),
-       map(users => {
-            let users_cd_remotelocker_requ = [];
-            users.forEach((user_primary_id) => {
-                this.restService.call<any>('/users/'+user_primary_id+'/requests')
-                    .subscribe(requests => {
-                        let first_remotelocker_request = true;
-                        requests.user_request
-                            .filter(ur => ur.pickup_location_circulation_desk =="cd_remotelocker-requ")
-                            .filter(ur => ur.task_name =="Transit Item")
-                            .forEach(request => {
-                                if (first_remotelocker_request) {
-                                    users_cd_remotelocker_requ.push({'user_barcode': user_primary_id, 'book_barcode': []});
-                                    first_remotelocker_request = false;
-                                } 
-                                for ( var index=0; index<users_cd_remotelocker_requ.length; index++ ) {
-                                    if (users_cd_remotelocker_requ[index]['user_barcode'] === user_primary_id) {
-                                        users_cd_remotelocker_requ[index]['book_barcode'].push(request.barcode)
-                                    }
-                                }
-                            });
+            map(printouts => this.__parse_unique_users_ids(printouts.printout)),
+            map(users => {
+                let users_cd_remotelocker_requ = [];
+                users.forEach((user_primary_id) => {
+                    this.restService.call<any>('/users/'+user_primary_id+'/requests')
+                        .subscribe(requests => {
+                            let books_barcodes = this.__fetch_books_barcodes(requests.user_request);
+                            if (books_barcodes.length !== 0) {
+                                users_cd_remotelocker_requ.push({'user_barcode': user_primary_id, 'book_barcode': books_barcodes})
+                            }
+                        })
                     })
-                })
-                this.apiResult =  users_cd_remotelocker_requ;
+                    this.apiResult =  users_cd_remotelocker_requ;
             })
         ).subscribe();
     }
