9ad911fda673c2751117dbc60ca3a429c72d3db3
Mateusz Lasota <mateusz.lasota@pg.edu.pl>
2023-12-29 10:57:05 +0100

    Remotelocker Component with user barcode according to the latest Robert concept with books descriptions and numeric showed barcode number

--- cloudapp/src/app/remotelocker/remotelocker.component.ts
+++ cloudapp/src/app/remotelocker/remotelocker.component.ts
@@ -1,5 +1,5 @@
-import { Observable, of } from 'rxjs';
-import { finalize, tap, map, flatMap } from 'rxjs/operators';
+import { Observable, of, forkJoin } from 'rxjs';
+import { finalize, tap, map, flatMap, mergeMap, concatMap } from 'rxjs/operators';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CloudAppRestService, CloudAppEventsService, Request, HttpMethod, 
   Entity, RestErrorResponse, AlertService } from '@exlibris/exl-cloudapp-angular-lib';
@@ -15,7 +15,8 @@ export class RemotelockerComponent implements OnInit, OnDestroy {
 
   loading = false;
   selectedEntity: Entity;
-  apiResult: any;
+  apiResult : any;
+  users_observable: []; 
 
   constructor(
     private restService: CloudAppRestService,
@@ -39,7 +40,7 @@ export class RemotelockerComponent implements OnInit, OnDestroy {
             return user_primary_id_span_html;
     }
   }
-  __parse_unique_users_ids(printouts: Array<any>): Array<number> {
+  __parse_unique_users_ids(printouts: Array<any>): Array<string> {
     let unique_users_ids = [];
     printouts.forEach((printout) => {
         let user_id = this.___parse_user_id(printout.letter, unique_users_ids)
@@ -49,34 +50,80 @@ export class RemotelockerComponent implements OnInit, OnDestroy {
     });
     return unique_users_ids.filter((value, index, array) => array.indexOf(value) === index);
   }
-  __fetch_books_barcodes(user_requests: Array<any>): Array<string> {
+  __select_books_descriptions(user_requests: Array<any>): Array<string> {
+    
     return user_requests
         .filter(ur => ur.pickup_location_circulation_desk === "cd_remotelocker-requ")
         .filter(ur => ur.task_name === "Transit Item")
-        .reduce((user_requests,book) => {user_requests.push(book.barcode); return user_requests;}, []);
+        .reduce((user_requests,book) => {console.log('book', book);user_requests.push({book_barcode: book.barcode, book_brief: book.title.substr(0,40) }); return user_requests;}, []);
+  }
+  __choice_user_barcode(user_datas:any): string 
+  {
+    console.log(user_datas);
+    let add_id_1 = user_datas.user_identifier.filter(ui => ui.id_type.value === 'OTHER_ID_1')[0]?.value;
+    let add_id_2 = user_datas.user_identifier.filter(ui => ui.id_type.value === 'OTHER_ID_2')[0]?.value;
+    if (add_id_2) {
+        if (user_datas.note_type.filter(nt => nt.note_text.value === "BARCODE").length !== 0
+            && user_datas.note_type.filter(nt => nt.note_text === "mLegitymacja").length !== 0) {
+                return add_id_2;
+        }
+    } else {
+        if (add_id_1) {
+            return add_id_1;
+        }
+    }
+    // brak identyfikatora dla książkomatu
+    if (user_datas.user_identifier.filter(ui => ui.id_type.value === 'BARCODE').length === 0) {
+        return 'brak identyfikatora dla książkomatu';
+    }
+    return user_datas.user_identifier.filter(ui => ui.id_type.value === 'BARCODE')[0].value;
   }
   _get_requested_resources():void {
-    let extracted_datas = [];
     this.restService.call<any>('/almaws/v1/task-lists/printouts?letter=Transit Letter&limit=100')
         .pipe(
             map(printouts => this.__parse_unique_users_ids(printouts.printout)),
-            map(users => {
-                let users_cd_remotelocker_requ = [];
-                users.forEach((user_primary_id) => {
-                    this.restService.call<any>('/users/'+user_primary_id+'/requests')
-                        .subscribe(requests => {
-                            let books_barcodes = this.__fetch_books_barcodes(requests.user_request);
-                            if (books_barcodes.length !== 0) {
-                                users_cd_remotelocker_requ.push({'user_barcode': user_primary_id, 'book_barcode': books_barcodes})
+            mergeMap(users => {
+                return forkJoin(users.map((user_primary_id) => {
+                    return this.restService.call<any>('/users/'+user_primary_id+'/requests').pipe(
+                        map(requests => {
+                            let books_descriptions = this.__select_books_descriptions(requests.user_request);
+                            if (books_descriptions.length !== 0) {
+                                return {
+                                        user_barcode: user_primary_id, 
+                                        books_descriptions: books_descriptions,
+                                       }
+                            } else {
+                                return null;
                             }
-                        })
+                        }),
+                        )
                     })
-                    console.log('sadsfafaf',users_cd_remotelocker_requ);
-                    this.apiResult =  users_cd_remotelocker_requ;
-            })
-        ).subscribe();
-    }
-
+                );
+            }),
+            mergeMap(user_requests => {
+                return forkJoin(user_requests.map((request) => {
+                    if (!request) return of(null);
+                    return this.restService.call<any>('/users/'+request.user_barcode).pipe(
+                        map(user_datas => {
+                            
+                            if (user_datas && user_datas?.external_id === "") {
+                                return {
+                                        user_barcode: this.__choice_user_barcode(user_datas), 
+                                        books_descriptions: request.books_descriptions
+                                       }
+                            } else {
+                                return null;
+                            }
+                        }),
+                    );
+                })
+            );}),
+            map(users_cd_requ => users_cd_requ.filter(notnull => notnull !== null)),
+        ).subscribe(result => {
+            this.apiResult = result;
+            console.log(this.apiResult);
+        });
+  }
   private tryParseJson(value: any) {
     try {
       return JSON.parse(value);
