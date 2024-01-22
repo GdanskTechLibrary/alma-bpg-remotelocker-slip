ce70373b1cfe4f2b92cf0e7d7d816dd266793317
Mateusz Lasota <mateusz.lasota@pg.edu.pl>
2024-01-22 08:05:29 +0100

    added condition for holdshelf

--- cloudapp/src/app/methods/submethods/forHoldshelf.ts
+++ cloudapp/src/app/methods/submethods/forHoldshelf.ts
@@ -1,7 +1,7 @@
 export function __passed_holdshelf(user_requests: Array<any>) {
  //        .filter(ur => ur.task_name === "Hold Shelf Request Slip Letter")//Transit Item")
     return user_requests
-//        .filter(ur => ur.pickup_location_circulation_desk === "cd_remotelocker-requ")
+        .filter(ur => ur.pickup_location_circulation_desk !== "cd_remotelocker-requ")
         .filter(ur => ur.task_name === "On Hold Shelf");
 }
 export function __extract_user_name_description(user_datas:any): string 
